311a97a4ff76366ab753370ad9014510af31c57d
neotixai <neotixx.ai@gmail.com>
2026-01-25 14:50:04 -0500

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   compress: true,
   poweredByHeader: false,
-  
-  // Optimisation des images
+
   images: {
     formats: ['image/avif', 'image/webp'],
   },
-  
-  // Cache plus agressif
+
   experimental: {
     optimizeCss: true,
   },
